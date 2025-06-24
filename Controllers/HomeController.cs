52d29b89c92e2d18f1b19bb0810e2fc797c06c5d
thienstyle04 <thien074605@gmail.com>
2025-06-24 17:41:07 +0700

    section3

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ComputerShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
